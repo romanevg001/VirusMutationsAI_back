0b2e99ae8ede3f29ecc3634a76a3f412e8cde130
Natali Donova <natali.donova@auriga.com>
2020-08-03 16:00:07 +0300

    +

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
+import { APP_INTERCEPTOR, APP_PIPE, APP_FILTER } from '@nestjs/core';
+import { GqlHttpExceptionFilter } from './shared/gql-http-error.filter';
+import { HttpErrorFilter } from './shared/http-error.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
+import { ValidationPipe } from './shared/validation.pipe';
+import { GraphQLModule } from '@nestjs/graphql';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register(),
+    // GraphQLModule.forRoot({
+    //   installSubscriptionHandlers: true,
+    //   typePaths: ['./**/*.graphql'],
+    //   context: ({req}) => ({ req }),
+    // }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {provide: APP_FILTER, useClass: HttpErrorFilter },
+ //   {provide: 'APP_FILTER_GQL', useClass: GqlHttpExceptionFilter },
+    {provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+    {provide: APP_PIPE, useClass: ValidationPipe },
+    AppService,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+
+import { ValidationPipe } from './shared/validation.pipe';
+import * as config from 'config';
+
+const serverConfig = config.get('server');
+const port = process.env.PORT || serverConfig.port;
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  if (process.env.MODE === 'development') {
+    logger.log(`enableCors in development mode`);
+    app.enableCors();
+  } else {
+    logger.log(`Accepting requests from origin "${serverConfig.origin}" `);
+    app.enableCors({
+      origin: serverConfig.origin,
+    });
+  }
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+  logger.log('Run on port:' + port);
+
 }
 bootstrap();
